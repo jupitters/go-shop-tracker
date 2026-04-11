36b2b8db888fd2314f1dd8e0605b6aa53d43d0d3
jupitters <sampaiobale@gmail.com>
2026-04-11 14:06:08 -0300

    order package

--- internal/models/order.go
+++ internal/models/order.go
@@ -2,4 +2,16 @@ package models
 
 var (
 	OrderStatus = []string{"Order placed", "Preparing", "Baking", "Quality check", "Ready"}
+	PizzaTypes  = []string{
+		"Margherita",
+		"Pepperoni",
+		"Vegetarian",
+		"Hawaiian",
+		"Bbq Chiken",
+		"Meat Lovers",
+		"Buffalo Chiken",
+		"Supreme",
+		"Truffle Mushroom",
+		"Four Cheese",
+	}
 )
