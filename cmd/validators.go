37c18441cf316573946e54c13870793483b8977e
jupitters <sampaiobale@gmail.com>
2026-04-13 13:27:56 -0300

    validators

--- cmd/validators.go
+++ cmd/validators.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"slices"
+
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator"
 	"github.com/jupitters/go-pizza-tracker/internal/models"
@@ -10,5 +12,11 @@ func RegisterCustomValidators() {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.registerValidation("valid_pizza_type", createSliceValidator(models.PizzaTypes))
 		v.registerValidation("valid_pizza_size", createSliceValidator(models.PizzaSizes))
+	}
+}
+
+func createSliceValidator(allowedVallues []string) validator.Func {
+	return func(fl validator.FieldValue) bool {
+		return slices.Contains(allowedValues, fl.Field().String())
 	}
 }
