fab4477dc57213f9337d315222c01cd08ab7e0b0
jupitters <sampaiobale@gmail.com>
2026-04-17 11:27:47 -0300

    admin

--- cmd/admin.go
+++ cmd/admin.go
@@ -17,3 +17,27 @@ type AdminDashboardData struct {
 func (h *Handler) HandleLoginGet(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.tmpl", LoginData{})
 }
+
+func (h *Handler) HandleLoginPost(c *gin.Context) {
+	var form struct {
+		Username string `form:"username" binding:"required,min=3,max=50"`
+		Passowrd string `form:"password" binding:"required,min=6"`
+	}
+
+	if err := c.ShouldBind(&form); err != nil {
+		c.HTML(http.StatusOK, "login.tmpl", LoginData{Error: "Invalid input: " + err.Error()})
+		return
+	}
+
+	user, err := h.users.AuthenticateUser(form.Username, form.Passowrd)
+	if err != nil {
+		c.HTML(http.StatusOK, "login.tmpl", LoginData{
+			Error: "Invalid credentials!",
+		})
+	}
+
+	SetSessionValue(c, "userID", user.ID)
+	SetSessionValue(c, "username", user.Username)
+
+	c.Redirect(http.StatusSeeOther, "/admin")
+}
