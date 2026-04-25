1db0265d8ffc5f69e749578a8b0f9b4e318ced96
jupitters <sampaiobale@gmail.com>
2026-04-24 21:31:14 -0300

    notification add client

--- cmd/notifications.go
+++ cmd/notifications.go
@@ -12,3 +12,14 @@ func NewNotificationManager() *NotificationManager {
 		clients: make(map[string]map[chan string]bool),
 	}
 }
+
+func (n *NotificationManager) AddClient(key string, client chan string) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if n.clients[key] == nil {
+		n.clients[key] = make(map[chan string]bool)
+	}
+
+	n.clients[key][client] = true
+}
